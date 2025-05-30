8e4f5b668ef69df604c3e60e90293e413c70fe1f
Madhan KJ <148443923+Madhan-KJ@users.noreply.github.com>
2025-05-30 22:08:36 +0530

    Update run.py
    updated host and port for online render deployment

--- Project/run.py
+++ Project/run.py
@@ -1,6 +1,9 @@
 from app import create_app
+import os
 
 app = create_app()
 
+port = int( os.environ.get("PORT") )
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=port)
